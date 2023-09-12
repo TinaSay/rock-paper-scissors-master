1c347a1ca1e5ff98fb212d238532d40364b9b28e
TinaSayapina <tina.sayapina@toptal.com>
2023-09-12 16:13:49 +0500

    Added animation keyframes

--- script.js
+++ script.js
@@ -42,7 +42,9 @@ const game=(choice)=>{
             break;
     }
 
-    console.log(result)
+   setTimeout(()=>{
+       battleView(choice,cChoice)
+   },400)
 }
 
 /**
@@ -73,4 +75,42 @@ overlay.addEventListener('click',(e)=>{
         overlay.style.opacity="0"
         modal.style.top="-1000%"
     })
+})
+
+/* You picked/ House picked */
+
+const yourChoiceImg=document.querySelector('.yourChoiceImg')
+const theirChoiceImg=document.querySelector('.theirChoiceImg')
+const playground=document.querySelector('.playground')
+const battle=document.querySelector('.battle')
+const darkcircle=document.querySelector('.darkcircle')
+
+const battleView=((choice,computerChoice)=>{
+    playground.style.display="none"
+    battle.style.display="flex"
+
+    setImg(choice,yourChoiceImg)
+
+
+    setTimeout(()=>{
+        darkcircle.style.display="none"
+        theirChoiceImg.style.display="block"
+        setImg(computerChoice,theirChoiceImg)
+    },3000)
+})
+
+
+const setImg=((choice,place)=>{
+    switch (choice){
+        case 'r':
+            place.src="images/rock.png"
+            break;
+        case 's':
+            place.src="images/scissors.png"
+            break;
+        case 'p':
+            place.src="images/paper.png"
+            break;
+    }
+
 })
